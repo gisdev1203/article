33ca22ff13aba9f6927dee4188bfb469f466f79b
Imran Ullah <imranullah21@gmail.com>
2023-09-15 21:15:32 +0500

    Milestone 1
    To implement article editing functionality, we have introduced a two-panel layout on the article editing page.
    
    The first panel displays the TinyMCE editor, where we have also included its comments plugin.
    
    The second panel contains required tabs. Currently, we have populated the "Fields" tab with a dropdown containing predefined values. In the future, we plan to integrate FormioJS to facilitate the creation of additional necessary fields.

--- ProjectTemplate/wwwroot/js/article.js
+++ ProjectTemplate/wwwroot/js/article.js
@@ -1,1 +1,15 @@
 ﻿
+tinymce.init({
+    selector: 'textarea',
+    plugins: 'tinycomments',
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+        { value: 'First.Name', title: 'First Name' },
+        { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject("See docs to implement AI Assistant"))
+});
+
+
